831d669496558f35ccaf0de57b07638c9386348d
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2018-10-20 12:58:03 +0200

    made constants in a better way i think

---
+++ src/fysik/pysics.java
@@ -0,0 +1,25 @@
+package fysik;
+
+public class pysics {
+
+		static double G = 9.82;
+		static double ATM = 101.3E3; // 101.3 kPa
+		static double R = 8.3144598;
+		static double p_0 = 101325;
+		static double c = 299792458;
+		static double g_swe = 9.82;
+	
+	
+	public static double fahrenheitToCelsius(double fahrenheit) {
+		
+	  return (fahrenheit -32) /1.8;
+	 
+	    
+	
+	}
+	
+	
+	
+	
+	
+}
