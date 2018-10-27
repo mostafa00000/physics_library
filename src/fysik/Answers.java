0915f694677b7c7036a334f034fac8919f730b58
mostafa00000 <42927262+mostafa00000@users.noreply.github.com>
2018-10-27 19:00:33 +0200

    some questions answered

---
+++ src/fysik/Answers.java
@@ -0,0 +1,34 @@
+package fysik;
+
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+public class Answers {
+
+public static void main(String[] args) {
+	
+	//uppgift 1 
+	
+	
+	//System.out.println(SolidTable.IRON.density * 0.08);
+
+	//uppgift 2
+	
+	//System.out.println(physics.svtDistance(2.8, 3600));
+	
+	//uppgift 3
+	
+	//System.out.println();
+
+	//uppgift 4
+	
+	//System.out.println((physics.pressureUnderWater(100) ) + "Pascal");
+	
+	//upgift 5
+	
+	//System.out.println(FluidTable.H2O.heatCapacity * 5 * 1); //?????
+	
+
+
+}
+
+}
